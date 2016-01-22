f44a8099d0cb4c787dbb4ee87d1d93725abddb7b
Rob Gaston <rgaston@fargeo.com>
2016-01-22 12:57:19 -0800

    fixing a thing

--- jan-16/www/js/index.js
+++ jan-16/www/js/index.js
@@ -33,7 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
         app.sqlLiteSetup();
     },
 
