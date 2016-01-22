99234aad0caf1b10cc9567b830c216853c15e4f5
Rob Gaston <rgaston@fargeo.com>
2016-01-22 14:54:37 -0800

    adding resources to map

--- jan-16/www/js/map.js
+++ jan-16/www/js/map.js
@@ -1,12 +1,35 @@
+var vectorSource = new ol.source.Vector();
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+});
+
 var map = new ol.Map({
     target: 'map',
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
-        })
+        }),
+        vectorLayer
     ],
     view: new ol.View({
         center: ol.proj.transform([-118.25,34.05], 'EPSG:4326', 'EPSG:3857'),
         zoom: 10
     })
 });
+
+document.addEventListener('deviceready', function () {
+    app.getResources(function(e,r) {
+      var resources = app.parseResources(r.rows);
+      for (var i = 0; i < resources.length; i++) {
+          var format = new ol.format.WKT();
+          var feature = format.readFeature(resources[i].geom, {
+              dataProjection: 'EPSG:4326',
+              featureProjection: 'EPSG:3857'
+          });
+          feature.set('resource', resources[i]);
+          vectorSource.addFeature(feature);
+      }
+      var extent = vectorSource.getExtent();
+      map.getView().fit(extent, map.getSize());
+    });
+}, false);
